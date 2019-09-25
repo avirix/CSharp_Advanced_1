58360b9e1bccc314b3357287ec3e1d012f8248ef
Alexander <sania.sviridenko@gmail.com>
2019-09-25 11:01:19 +0300

    add 4_Delegates

---
+++ 4_Delegates/IteaMessanger/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using static ITEA_Collections.Common.Extensions;
+
+namespace IteaDelegates.IteaMessanger
+{
+    public delegate void OnMessage(Message message);
+
+    public class Account
+    {
+        public string Username { get; private set; }
+        public List<Message> Messages { get; set; }
+        public OnMessage NewMessage { get; set; } = (message) =>
+        {
+            if (message.Send)
+                ToConsole($"{message.To.Username}, new message from {message.From.Username}:\n  {message.Preview}", ConsoleColor.Green);
+        };
+
+        public Account(string username)
+        {
+            Username = username;
+            Messages = new List<Message>();
+            //NewMessage += OnNewMessage;
+        }
+
+        public Message CreateMessage(string text, Account to)
+        {
+            var message = new Message(this, to, text);
+            Messages.Add(message);
+            return message;
+        }
+
+        public void Send(Message message)
+        {
+            message.Send = true;
+            message.To.Messages.Add(message);
+            message.To.NewMessage(message);
+        }
+
+        public void OnNewMessage(Message message)
+        {
+            if (message.Send)
+                ToConsole($"OnNewMessage: {message.From.Username}: {message.Preview}", ConsoleColor.DarkYellow);
+        }
+
+        public void ShowDialog(string username)
+        {
+            List<Message> messageDialog = Messages
+                .Where(x => x.To.Username.Equals(username) || x.From.Username.Equals(username))
+                .Where(x => x.Send)
+                .OrderBy(x => x.Created)
+                .ToList();
+            string str = $"Dialog with {username}";
+            ToConsole($"---{str}---");
+            foreach (Message message in messageDialog)
+            {
+                ToConsole($"{(message.From.Username.Equals(username) ? username : Username)}: {message.ReadMessage(this)}",
+                    message.From.Username.Equals(username) ? ConsoleColor.Cyan : ConsoleColor.DarkYellow);
+            }
+            ToConsole($"---{string.Concat(str.Select(x => "-"))}---");
+        }
+    }
+}
---
+++ 4_Delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IteaDelegates.IteaMessanger;
+
+namespace IteaDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Alex");
+            Account a2 = new Account("Ann");
+            a1.Send(a1.CreateMessage("Hi, Ann! How are you?", a2));
+            a1.Send(a1.CreateMessage("I'm Alex, from Kyiv", a2));
+            a2.Send(a2.CreateMessage("Hi, Alex! I'm from Lviv", a1));
+            a1.ShowDialog(a2.Username);
+        }
+    }
+}
