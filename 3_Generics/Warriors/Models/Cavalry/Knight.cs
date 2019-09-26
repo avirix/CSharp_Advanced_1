2e9427e8c89bf03316059ed6b419f522a5552bde
Sidorenko Andrey <sidoran@gmail.com>
2019-09-26 15:17:55 +0300

    Add Knights Class

---
+++ 3_Generics/Warriors/Models/Cavalry/Knight.cs
@@ -0,0 +1,20 @@
+﻿using StrategyGame.Base.Interfaces;
+using StrategyGame.Warriors.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyGame.Warriors.Models.Cavalry
+{
+    class Knight : CombatUnit
+    {
+        public Knight() : base(20, 10, CombatUnitType.Melee)
+        {
+        }
+
+        public override void MeleeAttack(IUnit unit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
