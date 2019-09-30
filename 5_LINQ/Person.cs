53fc8d2d559a34e22260a6e767878909f1e8ac8c
Alexander <sania.sviridenko@gmail.com>
2019-09-30 16:50:01 +0300

    add linq examples

---
+++ 5_LINQ/Person.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteaLinq
+{
+    public class Person : object
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Age}";
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
