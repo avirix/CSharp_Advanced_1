dd292453f82a415cdecb48f52fd727ebba4596f8
Sidorenko Andrey <sidoran@gmail.com>
2019-10-15 23:04:27 +0300

    Change services to async

--- 6_LINQ_to_SQL/Services/Interfaces/IService.cs
+++ 6_LINQ_to_SQL/Services/Interfaces/IService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using IteaLinqToSql.Models.Abstract;
 
 namespace IteaLinqToSql.Models.Interfaces
@@ -7,11 +8,17 @@ namespace IteaLinqToSql.Models.Interfaces
     public interface IService<T> where T : class, IIteaModel
     {
         BaseRepository<T> Repository { get; set; }
+        GenericAsyncRepository<T> AsyncRepository {get;set;}
         List<T> GetAll();
+        Task<List<T>> GetAllAsync();
         T FindById(int id);
+        Task<T> FindByIdAsync(int id);
         void Create(T item);
+        Task CreateAsync(T item);
         void Delete(T id);
+        Task DeleteAsync(T id);
         T Update(int id, T updatedItem);
+        Task<T> UpdateAsync(int id, T updateItem);
         IQueryable<T> GetQuery();
     }
 }
--- 6_LINQ_to_SQL/Services/UserService.cs
+++ 6_LINQ_to_SQL/Services/UserService.cs
@@ -1,30 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
 using IteaLinqToSql.Models.Abstract;
 using IteaLinqToSql.Models.Database;
 using IteaLinqToSql.Models.Entities;
 using IteaLinqToSql.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace IteaLinqToSql.Services
 {
     public class UserService : IService<User>
     {
         public BaseRepository<User> Repository { get; set; }
+        public GenericAsyncRepository<User> AsyncRepository { get; set; }
 
         public UserService(IteaDbContext dbContext)
         {
             Repository = new BaseRepository<User>(dbContext);
+            AsyncRepository = new GenericAsyncRepository<User>(dbContext);
         }
 
         public void Create(User item)
         {
             Repository.Create(item);
         }
+        public async Task CreateAsync(User item)
+        {
+            await AsyncRepository.CreateAsync(item);
+        }
 
         public void Delete(User item)
         {
             Repository.Remove(item);
+        }
+        public async Task DeleteAsync(User id)
+        {
+            await AsyncRepository.RemoveAsync(id);
         }
 
         public List<User> GetAll()
@@ -32,20 +43,40 @@ namespace IteaLinqToSql.Services
             return Repository.GetAll().ToList();
         }
 
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await AsyncRepository.GetAll().ToListAsync();
+        }
+
         public User Update(int id, User updatedItem)
         {
             Repository.Update(updatedItem);
             return updatedItem;
+        }
+        public async Task<User> UpdateAsync(int id, User updateItem)
+        {
+           await AsyncRepository.UpdateAsync(updateItem);
+           return updateItem;
         }
 
         public User FindById(int id)
         {
             return Repository.FindById(id);
         }
+        public async Task<User> FindByIdAsync(int id)
+        {
+           return await AsyncRepository.FindById(id);
+        }
 
         public IQueryable<User> GetQuery()
         {
             return Repository.GetAll();
+        }
+
+
+        public  IQueryable<User> GetQueryAsync()
+        {
+            return AsyncRepository.GetAll();
         }
     }
 }
